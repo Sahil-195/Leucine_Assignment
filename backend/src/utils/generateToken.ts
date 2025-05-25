8ab2bdc0c247983ba07af86649bb0df2bb5877af
Sahil Panjwani <sahilpanjwani195@gmail.com>
2025-05-26 02:58:00 +0530

    resolved token issues

--- backend/src/utils/generateToken.ts
+++ backend/src/utils/generateToken.ts
@@ -12,7 +12,7 @@ export const generateToken = (userId: Number, res: Response) => {
     res.cookie("token", token, {
         maxAge: 7 * 24 * 60 * 60 * 1000,
         httpOnly: true,
-        sameSite: "strict",
+        sameSite: "none",
         secure: process.env.NODE_ENV !== "development",
     });
 
