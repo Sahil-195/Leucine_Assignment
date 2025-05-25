6670fedfb87152b84f514c7f7f28bf099fe9d47e
Sahil Panjwani <sahilpanjwani195@gmail.com>
2025-05-26 02:02:19 +0530

    resolve cookie issue

--- frontend/src/api/axiosInstance.ts
+++ frontend/src/api/axiosInstance.ts
@@ -1,8 +1,20 @@
-import axios from 'axios';
+import axios, { AxiosHeaders } from 'axios';
+import { getCookieItem } from '../Utils/cookiesHelper';
 
 const axiosInstance = axios.create({
     baseURL: import.meta.env.VITE_REACT_APP_API_URL,
     withCredentials: true 
 });
+
+axiosInstance.interceptors.request.use(
+    (config) => {
+
+        config.withCredentials = true;
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
 
 export default axiosInstance;
